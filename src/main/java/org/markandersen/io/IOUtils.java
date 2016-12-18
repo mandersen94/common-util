33d8e0648267fdb7a2de7098d043415a44a7a6e3
mark@markandersen.org <mark@markandersen.org>
2016-12-18 09:37:51 -0600

    added gradle stuff

--- src/main/java/org/markandersen/io/IOUtils.java
+++ src/main/java/org/markandersen/io/IOUtils.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 
 public class IOUtils {
 
-	public static void closeQuietly(InputStream inputStream) {
+	publicssss static void closeQuietly(InputStream inputStream) {
 		if (inputStream == null) {
 			return;
 		}
